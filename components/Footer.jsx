20859805a406611e28a016428801f2322a23e35c
Barbara Szewczyk <comamysza@gmail.com>
2022-11-03 17:43:46 +0100

    Adds footer

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<footer>
+			<a href='https://github.com/bhszewczyk'>
+				<i class='fa-brands fa-square-github icon-social'></i>
+			</a>
+			<a href='https://www.instagram.com/hiiriart/'>
+				<i class='fa-brands fa-square-instagram icon-social'></i>
+			</a>
+		</footer>
+	);
+}
+
+export default Footer;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Information from '../components/Information';
 import MainContent from '../components/MainContent';
+import Footer from '../components/Footer';
 
 function App() {
 	return (
 		<div className='app'>
 			<Information />
 			<MainContent />
+			<Footer />
 		</div>
 	);
 }
